34d09b62fa46d905773e8aaf67f4b0275e7cf462
KouthairBizid1 <kouthairbizid@gmail.com>
2026-02-24 22:16:01 +0100

    migration added

--- migrations/Version20260224211516.php
+++ migrations/Version20260224211516.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260224205643 extends AbstractMigration
+final class Version20260224211516 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,20 +20,30 @@ final class Version20260224205643 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE medecin (id INT AUTO_INCREMENT NOT NULL, profile_id INT DEFAULT NULL, specialite_id INT DEFAULT NULL, date_embauche DATE DEFAULT NULL, INDEX IDX_1BDA53C6CCFA12B8 (profile_id), INDEX IDX_1BDA53C62195E0F0 (specialite_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE patient (id INT AUTO_INCREMENT NOT NULL, profile_id INT DEFAULT NULL, date_inscription DATE DEFAULT NULL, INDEX IDX_1ADAD7EBCCFA12B8 (profile_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE profil (id INT AUTO_INCREMENT NOT NULL, cin VARCHAR(8) NOT NULL, name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, role VARCHAR(255) NOT NULL, image VARCHAR(255) NOT NULL, tel VARCHAR(8) NOT NULL, sexe VARCHAR(10) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE rendez_vous (id INT AUTO_INCREMENT NOT NULL, patient_id INT DEFAULT NULL, medecin_id INT DEFAULT NULL, date DATETIME NOT NULL, duree INT DEFAULT 30, motif VARCHAR(255) DEFAULT NULL, etat VARCHAR(20) DEFAULT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, INDEX IDX_65E8AA0A6B899279 (patient_id), INDEX IDX_65E8AA0A4F31A84 (medecin_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE specialite (id INT AUTO_INCREMENT NOT NULL, labelle VARCHAR(30) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0E3BD61CE16BA31DBBF396750 (queue_name, available_at, delivered_at, id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE medecin ADD CONSTRAINT FK_1BDA53C6CCFA12B8 FOREIGN KEY (profile_id) REFERENCES profil (id) ON DELETE SET NULL');
+        $this->addSql('ALTER TABLE medecin ADD CONSTRAINT FK_1BDA53C62195E0F0 FOREIGN KEY (specialite_id) REFERENCES specialite (id)');
+        $this->addSql('ALTER TABLE patient ADD CONSTRAINT FK_1ADAD7EBCCFA12B8 FOREIGN KEY (profile_id) REFERENCES profil (id) ON DELETE SET NULL');
         $this->addSql('ALTER TABLE rendez_vous ADD CONSTRAINT FK_65E8AA0A6B899279 FOREIGN KEY (patient_id) REFERENCES patient (id)');
         $this->addSql('ALTER TABLE rendez_vous ADD CONSTRAINT FK_65E8AA0A4F31A84 FOREIGN KEY (medecin_id) REFERENCES medecin (id)');
-        $this->addSql('ALTER TABLE medecin ADD CONSTRAINT FK_1BDA53C62195E0F0 FOREIGN KEY (specialite_id) REFERENCES specialite (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE medecin DROP FOREIGN KEY FK_1BDA53C6CCFA12B8');
         $this->addSql('ALTER TABLE medecin DROP FOREIGN KEY FK_1BDA53C62195E0F0');
+        $this->addSql('ALTER TABLE patient DROP FOREIGN KEY FK_1ADAD7EBCCFA12B8');
         $this->addSql('ALTER TABLE rendez_vous DROP FOREIGN KEY FK_65E8AA0A6B899279');
         $this->addSql('ALTER TABLE rendez_vous DROP FOREIGN KEY FK_65E8AA0A4F31A84');
+        $this->addSql('DROP TABLE medecin');
+        $this->addSql('DROP TABLE patient');
+        $this->addSql('DROP TABLE profil');
         $this->addSql('DROP TABLE rendez_vous');
         $this->addSql('DROP TABLE specialite');
         $this->addSql('DROP TABLE messenger_messages');
